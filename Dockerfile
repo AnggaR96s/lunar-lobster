7bf29ec8d69cb3427e7d58b706d0c5bcf58c5503
Angga <angga@linuxmail.org>
2022-11-17 09:36:47 +0000

    lunar-lobster: initial commit
    Change-Id: Iaa432398b3909cda5ff106bcadf58a0a5a1ae38b
    Signed-off-by: Angga <angga@linuxmail.org>

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM scratch
+ADD ubuntu-lunar-core-cloudimg-amd64-root.tar.gz /
+CMD ["bash"]
---
+++ lunar.sh
@@ -0,0 +1,19 @@
+rm MD5SUMS
+rm MD5SUMS.gpg
+rm SHA1SUMS
+rm SHA1SUMS.gpg
+rm SHA256SUMS
+rm SHA256SUMS.gpg
+rm build-info.txt
+rm ubuntu-lunar-core-cloudimg-amd64-root.tar.gz
+rm ubuntu-lunar-core-cloudimg-amd64.manifest
+
+wget https://partner-images.canonical.com/core/lunar/current/MD5SUMS
+wget https://partner-images.canonical.com/core/lunar/current/MD5SUMS.gpg
+wget https://partner-images.canonical.com/core/lunar/current/SHA1SUMS
+wget https://partner-images.canonical.com/core/lunar/current/SHA1SUMS.gpg
+wget https://partner-images.canonical.com/core/lunar/current/SHA256SUMS
+wget https://partner-images.canonical.com/core/lunar/current/SHA256SUMS.gpg
+wget https://partner-images.canonical.com/core/lunar/current/ubuntu-lunar-core-cloudimg-amd64-root.tar.gz
+wget https://partner-images.canonical.com/core/lunar/current/ubuntu-lunar-core-cloudimg-amd64.manifest
+wget https://partner-images.canonical.com/core/lunar/current/unpacked/build-info.txt
